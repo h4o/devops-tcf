d369bc23c78a49ae3c922ad64b3a86670c6449f7
Fabien Vicente <fvicente@polytech.unice.fr>
2016-03-18 08:18:03 +0100

    rework

---
+++ j2e/catalogue/src/tst/java/arquillian/AbstractTCFTest.java
@@ -0,0 +1,23 @@
+package arquillian;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractTCFTest {
+
+
+	@EJB
+	protected Database memory;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+}
---
+++ j2e/customer/src/tst/java/arquillian/AbstractTCFTest.java
@@ -0,0 +1,34 @@
+package arquillian;
+
+import fr.unice.polytech.isa.tcf.modules.cart.CartModifier;
+import fr.unice.polytech.isa.tcf.modules.cart.components.CartBean;
+import fr.unice.polytech.isa.tcf.modules.cart.components.carts.CartStatefulBean;
+import fr.unice.polytech.isa.tcf.modules.customer.entities.Customer;
+import fr.unice.polytech.isa.tcf.modules.customer.exceptions.AlreadyExistingCustomerException;
+import fr.unice.polytech.isa.tcf.modules.cart.interceptors.Logger;
+import fr.unice.polytech.isa.tcf.modules.database.Database;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractTCFTest {
+
+
+	@EJB
+	protected Database memory;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				// Utils
+				.addPackage(Database.class.getPackage())
+				// Entities
+				.addPackage(Customer.class.getPackage());
+	}
+
+}
