897af9c7db6f985566eca6b2e1de81464c02e7da
Fabien Vicente <fvicente@polytech.unice.fr>
2016-03-31 16:10:29 +0200

    it's repaired !

--- j2e/business/src/main/java/fr/unice/polytech/isa/tcf/modules/business/entities/Cookies.java
+++ j2e/business/src/main/java/fr/unice/polytech/isa/tcf/modules/business/entities/Cookies.java
@@ -1,9 +1,6 @@
 package fr.unice.polytech.isa.tcf.modules.business.entities;
 
 public enum Cookies {
-	//okay now let's do something stupid:
-	//I'm going to break the module used by all the project
-	*ùlflc
 	CHOCOLALALA("Chocolalala", 1.30),
 	DARK_TEMPTATION("Dark Temptation", 1.90),
 	SOO_CHOCOLATE("Soo Chocolate", 1.25);
