36bc21f03a243b17f9ac83c057b6b0f552e76708
Fabien Vicente <fvicente@polytech.unice.fr>
2016-03-31 10:59:09 +0200

    dummy change to trigger build

--- j2e/business/src/main/java/fr/unice/polytech/isa/tcf/modules/business/entities/Cookies.java
+++ j2e/business/src/main/java/fr/unice/polytech/isa/tcf/modules/business/entities/Cookies.java
@@ -1,7 +1,7 @@
 package fr.unice.polytech.isa.tcf.modules.business.entities;
 
 public enum Cookies {
-
+	//let's compile it to force the changes
 	CHOCOLALALA("Chocolalala", 1.30),
 	DARK_TEMPTATION("Dark Temptation", 1.90),
 	SOO_CHOCOLATE("Soo Chocolate", 1.25);
